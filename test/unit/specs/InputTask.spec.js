7a2228992aeaee00646945d2129a908917f8cc5a
Vinicius Barizon <vinicius.barizon@oi.net.br>
2018-02-27 17:07:33 -0300

    TaskList and Test

---
+++ test/unit/specs/InputTask.spec.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import InputTask from '@/components/InputTask'
+
+describe('InputTask.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(InputTask)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelectorAll('.new-todo').length).to.equal(1)
+  })
+})
